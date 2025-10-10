28f69c448a8bca6700b5394d764d645e79676349
Estell Moore <estell.moore.13@gmail.com>
2025-10-10 15:37:54 -0600

    Adding test and exceptions

--- src/main/com/month/Month.java
+++ src/main/com/month/Month.java
@@ -9,8 +9,12 @@ public class Month<T> {
 
     // getter and setter
     public void setMonth(T month) {
+        if (month == null) {
+            throw new NullPointerException("Month cannot be null!");
+        }
         this.month = month;
     }
+
     public T getMonth() {
         return month;
     }
